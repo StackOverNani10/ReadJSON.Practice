ff76ac82f500e48c6d1901d10e100a3933e14c36
Daniel Domínguez <1097711@est.intec.edu.do>
2022-12-06 21:52:58 -0400

    ajuste

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-//import screens from "../screens/characterEntry.json";
+import screens from "../screens/characterEntry.json";
 
 console.log("Captura de Caracteres");
 
+/*
 const nombres = "Herald";
 const apellidos = "Jumping";
 const puntuacion = "88";
 const habilidad = "Super Salto";
 
 const newCharacter = new characterSchema({nombres, apellidos, puntuacion, habilidad});
+*/
 
+var objetoJson = JSON.stringify(screens)
 
-console.log(newCharacter);
+console.log(objetoJson);
